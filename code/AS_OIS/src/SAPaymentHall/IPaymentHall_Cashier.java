44024c66d797386570fbd52e01eda07922b3e1c6
Tom1k <tomascosta@ua.pt>
2021-04-20 00:40:25 +0100

    Rounded changes + PaymentHall

--- code/AS_OIS/src/SAPaymentHall/IPaymentHall_Cashier.java
+++ code/AS_OIS/src/SAPaymentHall/IPaymentHall_Cashier.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package SACorridorHall;
-
+package SAEntranceHall;
 
 /**
  *
  * @author omp
  */
-public interface ICorridorHall_Manager {
+public interface IEntranceHall_Manager {
+    public void call();
 }
---
+++ code/AS_OIS/src/SAPaymentHall/SAPaymentHall.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SAEntranceHall;
+import Communication.NotifyCustomerState;
+
+import FIFO.FIFO;
+import Main.OIS_GUI;
+
+/**
+ *
+ * @author omp
+ */
+public class SAEntranceHall implements IEntranceHall_Customer,
+                                       IEntranceHall_Manager,
+                                       IEntranceHall_Control {
+    
+    final FIFO fifoEntranceHall;
+    private int totalCustomers;
+    private final OIS_GUI GUI;
+    private final int customersPosition[];
+    private final NotifyCustomerState notify;
+
+    public SAEntranceHall( int maxCustomers, OIS_GUI GUI, NotifyCustomerState notify ) {
+        this.fifoEntranceHall = new FIFO(maxCustomers);
+        this.GUI = GUI;
+        this.totalCustomers = maxCustomers;
+        this.customersPosition = new int[totalCustomers];
+        for(int i = 0; i < totalCustomers; i ++){
+            this.customersPosition[i] = -1;
+        }
+        this.notify = notify;
+    }
+    
+    @Override
+    public void call() {
+        while (true) {
+            fifoEntranceHall.out();
+        }
+    }
+
+    @Override
+    public void in(int customerId) {
+        notify.sendCustomerState("EntranceHall", customerId);
+        int position = this.selectPositionInGUI(customerId);
+        GUI.moveCustomer(customerId, new Integer[] {1, position});
+        int customerLeaving = fifoEntranceHall.in(customerId);
+        System.out.println("Customer " + customerLeaving + " leaving EntranceHall.");   
+    }
+   
+    private int selectPositionInGUI(int customerId){
+        int position = (int) Math.round((Math.random() * (totalCustomers - 1)));
+       
+        while(customersPosition[position] != -1) position = (int) Math.round((Math.random() * (totalCustomers - 1)));
+       
+        customersPosition[position] = customerId;
+        return position;
+    } 
+}
